10eaf574d211610b472c27d4660539300f5a4ec4
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-06 10:11:08 -0600

    creates feature tests for job-tag features

---
+++ spec/features/jobs/user_sees_tags_and_counts_on_specific_job_page.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe "As a user" do
+  context "on a specific job page" do
+    it "can see a tags associated with job" do
+      job = create(:job_with_tags, tag_count: 2)
+      tag1 = job.tags.first
+      tag2 = job.tags.first
+
+      visit job_path(job)
+
+      expect(page).to have_content(tag1.name)
+      expect(page).to have_content(tag2.name)
+    end
+
+    it "can see a count of jobs per tags for each associated with job" do
+      job = create(:job_with_tags, tag_count: 2)
+      tag1 = job.tags.first
+      tag2 = job.tags.first
+      job2 = create(:job)
+      job3 = create(:job)
+      job2.tags << tag1
+      job2.tags << tag2
+      job3.tags << tag1
+
+      visit job_path(job)
+
+      expect(page).to have_content("#{tag1.name}: 3")
+      expect(page).to have_content("#{tag2.name}: 2")
+    end
+  end
+end
