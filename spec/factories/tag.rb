27d9887e5a1c22521c3ef6b5e7028184c562edfa
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-06-06 10:41:24 -0600

    creates tags with jobs factory

--- spec/factories/tag.rb
+++ spec/factories/tag.rb
@@ -5,6 +5,17 @@ FactoryGirl.define do
   
   factory :tag do
     name 
+    
+    factory :tag_with_jobs do
+      
+      transient do
+        job_count 2
+      end
+
+      after(:create) do |tag, evaluator|
+        tag.jobs << create_list(:job, evaluator.job_count)
+      end
+    end
   end
 end
 
